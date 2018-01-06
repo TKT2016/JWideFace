2eb15907ad345860ab8de2de5b56d17272fdf50e
TKT2016 <bailanzi@aliyun.com>
2018-01-06 12:50:13 +0800

    a

--- src/org/wideface/Imp/models/FaceCodeModel.java
+++
@@ -1,6 +0,0 @@
-package org.wideface.Imp.models;
-
-public class FaceCodeModel {
-    public String[] Names;
-    public boolean IgnoreCase;
-}
--- src/test/t3/Hello.java
+++
@@ -1,7 +0,0 @@
-package test.t3;
-
-public class Hello {
-    public void say() {
-        System.out.println("Hello");
-    }
-}
